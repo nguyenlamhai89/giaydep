62b331c6d21d69ac83ad722f1742ca0cebed722b
NGUYENLAMHAI89\Asus <nguyenlamhai89@gmail.com>
2017-10-07 21:14:15 +0700

    no message

--- app.py
+++ app.py
@@ -20,5 +20,16 @@ for shoes  in NLHStore.objects():
 def index():
     return render_template('index.html', shoeslist=NLHStore.objects())
 
+@app.route("/admin")
+def admin():
+    return render_template("admin.html", shoeslist=NLHStore.objects())
+
+@app.route("/deleteshoes/<shoes_id>")
+def deleteshoes(shoes_id):
+    shoes = NLHStore.objects().with_id(shoes_id)
+    if shoes is not None:
+        shoes.delete()
+    return redirect("/admin")
+
 if __name__ == '__main__':
   app.run(debug=True)
